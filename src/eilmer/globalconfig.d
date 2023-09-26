9616bba7193456c2e2bc77638fd192b6cdd56d2a
Nick N. Gibbons <n.gibbons@uq.edu.au>
2023-09-26 10:17:23 +1000

    eilmer: Added a novel shock detector based on the HLLC wave speeds

--- src/eilmer/globalconfig.d
+++ src/eilmer/globalconfig.d
@@ -506,6 +506,7 @@ ResidualSmoothingType residual_smoothing_type_from_name(string name)
 // Symbolic names for the shock detectors
 enum ShockDetector {
     PJ,
+    NNG,
 }
 
 @nogc
@@ -513,6 +514,7 @@ string shock_detector_name(ShockDetector sd)
 {
     final switch ( sd ) {
     case ShockDetector.PJ: return "PJ";
+    case ShockDetector.NNG: return "NNG";
     }
 }
 
@@ -521,6 +523,7 @@ ShockDetector shock_detector_from_name(string name)
 {
     switch ( name ) {
     case "PJ": return ShockDetector.PJ;
+    case "NNG": return ShockDetector.NNG;
     default:
         throw new FlowSolverException("Invalid shock detector name: ");
     }
